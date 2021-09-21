ccf68e37b310b4f57503fc1f43e52d5d65cdc7e6
Ka4mar <Berserk_ka4mar@mail.ru>
2021-09-21 19:01:53 +0300

    D/Z

---
+++ backend/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class User(models.Model):
+    user_name = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.EmailField(verbose_name='email address', max_length=255, unique=True)
